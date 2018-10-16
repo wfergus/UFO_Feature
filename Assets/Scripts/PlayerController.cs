c1222d3dc912f6440272d557a34c06f83b7ac02f
Will <wfergus@comcast.net>
2018-10-16 12:35:55 -0500

    adds the copying of base UFO game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float speed;
+    private int count = 0;
+    public Text countText;
+    public Text winText;
+    void Update()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        winText.text = "";
+        SetCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 6)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
